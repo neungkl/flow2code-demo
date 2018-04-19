fc1177f3a6d094ff14960613f2d5d43c45fcd214
Kosate Limpongsa <kosatelim@gmail.com>
2018-04-19 21:04:32 +0700

    Add model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, abort, redirect, url_for, request
 from werkzeug.utils import secure_filename
+from model import flow2code
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'upload'
@@ -8,25 +9,52 @@ UPLOAD_FOLDER = 'upload'
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in set(['jpg', 'png', 'jpeg'])
 
+@app.route('/list')
+def dir_listing():
+    abs_path = os.path.join(os.getcwd(), "upload")
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
+
 @app.route("/upload", methods=['POST'])
 def upload():
   if request.method == 'POST':
     if 'flowchart' not in request.files:
-      return("No file part.")
+      return "No file part."
 
     file = request.files['flowchart']
     if file.filename == '':
-      return("No selected file.")
+      return "No selected file."
     if file and allowed_file(file.filename):
       filename = secure_filename(file.filename)
       file.save(os.path.join(os.getcwd(), UPLOAD_FOLDER, filename))
-      return("Correct")
+      return redirect(url_for('flow', img_path=filename))
     else:
       return "File is not image."
 
 @app.route("/")
 def root():
   return render_template('index.html')
+
+@app.route("/flow/<img_path>")
+def flow(img_path):
+  img_full_path = os.path.join(os.getcwd(), UPLOAD_FOLDER, img_path)
+  if not os.path.isfile(img_full_path):
+    return abort(404)
+
+  result = flow2code(img_full_path)
+  return render_template(
+    'flow.html',
+    tokens=result["tokens"],
+    positions=result["positions"].tolist(),
+    img_path=img_path
+  )
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
---
+++ model.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+
+from PIL import Image
+
+PADDING_SIZE = 20
+IMAGE_SIZE = 256
+BATCH_SIZE = 4
+
+ALL_WORD = ['<none>', '', 'statement', 'if', 'else', 'elseif', 'while', 'end', '<START>', '<END>']
+N_ONEHOT_WORD = len(ALL_WORD)
+CHARS_MAP = {v: k for k, v in enumerate(ALL_WORD)}
+
+from keras.layers import Input, Dense, Flatten, Concatenate, Conv2D, MaxPooling2D, LSTM, RepeatVector, Embedding
+from keras.layers import Reshape, UpSampling2D
+from keras.optimizers import Adam
+from keras.models import Model
+from keras import backend as K
+import tensorflow as tf
+
+def generate_model():
+  word_input = Input(batch_shape=(BATCH_SIZE, PADDING_SIZE))
+  image_input = Input(batch_shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 1))
+  
+  img = Conv2D(16, (3, 3), padding='same', activation='relu')(image_input)
+
+  img = MaxPooling2D()(img)
+  img = MaxPooling2D()(img)
+  img = MaxPooling2D()(img)
+  img = MaxPooling2D()(img)
+  img = MaxPooling2D()(img)
+
+  img = Flatten()(img)
+  img = Dense(64)(img)
+  img = RepeatVector(PADDING_SIZE)(img)
+
+  w = Embedding(N_ONEHOT_WORD, 64)(word_input)
+
+  x = Concatenate()([w, img])
+  x = LSTM(32)(x)
+
+  y = Dense(32)(x)
+  y = Dense(N_ONEHOT_WORD, activation='softmax', name='y_word')(y)
+
+  y_pos = Dense(32)(x)
+  y_pos = Dense(2, name='y_position')(y_pos)
+
+  optimizer = Adam(lr=0.0001)
+  model = Model(inputs=[word_input, image_input], outputs=[y_pos, y])
+  
+  model.compile(optimizer=optimizer,
+                loss=['mse', 'categorical_crossentropy'],
+                metrics={'y_position':'mse', 'y_word':'accuracy'})
+  
+  return model
+
+model = None
+
+def convert_to_feature_list(feature_words):
+  onehots = []
+  for word in feature_words:
+    onehots.append(CHARS_MAP[word])
+  return np.array(onehots)
+
+def read_image(image_file):
+  image = Image.open(image_file).convert('L')
+  width, height = image.size
+  image = image.convert()
+  image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
+  image = np.asarray(image).reshape((IMAGE_SIZE, IMAGE_SIZE, 1))
+  return 1 - (image / 255), width, height
+
+def flow2code(image_path):
+  global model
+
+  if model is None:
+    print("Reload model !!!")
+    model = generate_model()
+    model.load_weights("./model/model-weight-test.hdf5")
+  
+  image, img_width, img_height = read_image(image_path)
+
+  tokens = np.zeros((BATCH_SIZE, PADDING_SIZE))
+  images = np.zeros((BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 1))
+  results = []
+  position = []
+  
+  token = [''] * (PADDING_SIZE + 1)
+  
+  for i in range(BATCH_SIZE):
+    images[i] = image
+  
+  for i in range(20):
+    token = token[1:]
+    
+    tokens[BATCH_SIZE - 1] = convert_to_feature_list(token)
+    
+    pos, predict = model.predict([tokens, images])
+    predict_token = ALL_WORD[np.argmax(predict[BATCH_SIZE - 1])]
+    results.append(predict_token)
+    
+    token += [predict_token]
+    position += [pos[BATCH_SIZE - 1]]
+    
+    if predict_token == "<END>":
+        break
+
+  position = np.array(position)
+  position[position[:,0] > 0,0] *= img_width / 10
+  position[position[:,1] > 0,1] *= img_height / 10
+
+  return {
+    "tokens": results,
+    "positions": position,
+    "img_width": img_width,
+    "img_height": img_height
+  }
